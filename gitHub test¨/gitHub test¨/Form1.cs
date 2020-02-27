50d090b4884e3c74120416eb6d0b5ee8b57eca32
Scott Tollebäck <scotol0230@skynet.halmstad.se>
2020-02-27 19:14:31 +0100

    Github test

---
+++ gitHub test¨/gitHub test¨/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gitHub_test_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Metod();
+        }
+        void Metod()
+        {
+            double radie = double.Parse(textBox1.Text);
+            double area = (radie * radie) * Math.PI;
+            textBox2.Text = area.ToString();
+        }
+
+    }
+}
